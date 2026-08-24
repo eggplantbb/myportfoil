ef91c271e5a99b9224f89a1e57487e0b19b9aa14
chawmin <303786880@qq.com>
2026-08-24 20:27:20 +0800

    remove unused png assets and hide travel tab

--- src/content/tabs/index.ts
+++ src/content/tabs/index.ts
@@ -1,11 +1,9 @@
 import { aiTab } from './ai';
 import { designTab } from './design';
 import { helloWorldTab } from './helloWorld';
-import { travelTab } from './travel';
 
 export const tabs = [
   helloWorldTab,
   designTab,
   aiTab,
-  travelTab,
 ];
